4266afd49129609ddeb88ff70ed21ec887a2260a
ricardo.shinoda <ricardoshinoda@gmail.com>
2025-12-18 20:48:19 -0300

    adding aula 39

--- Script.sql
+++ Script.sql
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 -- bringing my sql study from Github
-=======
--- aula 43 (comandos adicionais).
--- testando nova conexão
->>>>>>> 662ccbd3a692b02fe57efba11a20795f3d24d92c
+/Users/ricardo/Library/DBeaverData/workspace6/General/Scripts/Script.sql
