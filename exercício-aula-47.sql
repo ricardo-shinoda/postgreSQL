e7ae196dcf6e584841092f7856ff3a49bac21cb4
ricardo.shinoda <ricardoshinoda@gmail.com>
2026-01-09 21:23:59 -0300

    Terminando exercicio 47

--- exercício-aula-47.sql
+++ exercício-aula-47.sql
@@ -39,9 +39,26 @@ FROM cliente clt
 WHERE idmunicipio = (SELECT idmunicipio FROM transportadora trn WHERE clt.idmunicipio = trn.idmunicipio);
 
 --6. Atualizar o valor do pedido em 5% para os pedidos que o somatório do valor total dos produtos daquele pedido seja maior que a média do valor total
---
 --de todos os produtos de todos os pedidos.
---
+SELECT
+	idpedido,
+	valor,
+	valor + (valor * 0.05) AS "+5%",
+	(SELECT avg(valor) FROM pedido) AS "media"
+FROM pedido
+WHERE valor > (SELECT avg(valor) FROM pedido);
+
 --7. O nome do cliente e a quantidade de pedidos feitos pelo cliente.
---
+SELECT
+	nome,
+	(SELECT count(idcliente) FROM pedido pdd WHERE cln.idcliente = pdd.idcliente) 
+FROM cliente cln;
+
 --8. Para revisar, refaça o exercício anterior (número 07) utilizando group by e mostrando somente os clientes que fizeram pelo menos um pedido.
+SELECT
+	cln.nome,
+	(SELECT count(idcliente) FROM pedido pdd WHERE cln.idcliente = pdd.idcliente) AS "Quantidade"
+FROM cliente cln
+WHERE (SELECT count(idcliente) FROM pedido pdd WHERE cln.idcliente = pdd.idcliente) > 0
+GROUP BY cln.nome, cln.idcliente;
+
