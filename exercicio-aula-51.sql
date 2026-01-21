922e63e74b04ef59567cf4c985eeb4ae8c063388
ricardo.shinoda <ricardoshinoda@gmail.com>
2026-01-20 21:36:07 -0300

    Exercicio 1 - aula 51

--- exercicio-aula-51.sql
+++ exercicio-aula-51.sql
@@ -2,7 +2,28 @@
 
 --1. O nome, a profissão, a nacionalidade, o complemento, o município, a unidade de federação, o bairro, 
 -- o CPF,o RG, a data de nascimento, o gênero (mostrar “Masculino” ou “Feminino”), o logradouro, o número e as observações dos clientes.
-SELECT * FROM cliente;
+CREATE VIEW dado_cliente AS
+SELECT
+	cln.nome AS cliente,
+	prf.nome AS profissão,
+	ncl.nome AS nacionalidade,
+	cpl.nome AS complemento,
+	mun.nome AS município,
+	u.nome AS uf,
+	bai.nome AS bairro,
+	cln.cpf AS cpf,
+	cln.rg AS rg,
+	cln.data_nascimento AS nascimento,
+	cln.genero AS Gênero
+FROM cliente cln
+LEFT JOIN profissao prf ON cln.idprofissao = prf.idprofissao
+LEFT JOIN nacionalidade ncl ON cln.idnacionalidade = ncl.idnacionalidade
+LEFT JOIN complemento cpl ON cln.idcomplemento = cpl.idcomplemento
+LEFT JOIN municipio mun ON cln.idmunicipio = mun.idmunicipio
+LEFT JOIN uf u ON cln.iduf = u.iduf
+LEFT JOIN bairro bai ON cln.idbairro = bai.idbairro;
+
+SELECT * FROM dado_cliente WHERE Cliente = 'Amanda';
 
 --2. O nome do município e o nome e a sigla da unidade da federação.
 --
