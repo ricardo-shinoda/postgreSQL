e9aeafddeb5edb22ecdee6b9872bc04f873e7cd9
ricardo.shinoda <ricardoshinoda@gmail.com>
2026-01-24 06:44:30 -0300

    Finishing exercise 51 - updating table with autoincrement

--- exercise-55-autoincrement.sql
+++ exercise-55-autoincrement.sql
@@ -1,24 +1,19 @@
 -- Auto increment exercise
 
 --1. Create sequences for all other tables in the database
+
 --a. Customer
-
-SELECT * FROM customer;
 SELECT max(customer_id) + 1 FROM customer;
-
 CREATE SEQUENCE customer_id_seq MINVALUE 17;
 ALTER TABLE customer ALTER customer_id SET DEFAULT nextval('customer_id_seq');
 ALTER SEQUENCE customer_id_seq OWNED BY customer.customer_id;
 
 --b Adding autoincrement function to a column in table complement
 
-
 INSERT INTO address_complement (name) VALUES ('Cabin');
 
 
 --c. Supplier
-SELECT * FROM supplier;
-
 SELECT max(supplier_id) + 1 FROM supplier;
 CREATE SEQUENCE supplier_id_seq MINVALUE 5;
 ALTER TABLE supplier ALTER supplier_id SET DEFAULT nextval('supplier_id_seq');
@@ -29,8 +24,6 @@ INSERT INTO supplier (company_name, contact_name, phone, email, city_id)
 VALUES ('Linux', 'Lucas Shinoda', '(14)0000-1111', 'lucas@nasa.com', 4);
 
 --d. City
-SELECT * FROM city;
-
 SELECT max(city_id) + 1 FROM city;
 CREATE SEQUENCE city_id_seq MINVALUE 11;
 ALTER TABLE city ALTER city_id SET DEFAULT nextval('city_id_seq');
@@ -40,8 +33,6 @@ INSERT INTO city (name) VALUES ('Bauru');
 UPDATE city SET state_id = 2 WHERE name = 'Bauru';
 
 --e. Nationality
-SELECT * FROM nationality;
-
 SELECT max(nationality_id) + 1 FROM nationality;
 CREATE SEQUENCE nationality_id_seq MINVALUE 5;
 ALTER TABLE nationality ALTER nationality_id SET DEFAULT nextval('nationality_id_seq');
@@ -50,8 +41,6 @@ ALTER SEQUENCE nationality_id_seq OWNED BY nationality.nationality_id;
 INSERT INTO nationality (name) VALUES ('Japanese');
 
 --f. Order
-SELECT * FROM orders;
-
 SELECT max(order_id) + 1 FROM orders; -- to get the next order_id value available.
 CREATE SEQUENCE order_id_seq MINVALUE 16;
 ALTER TABLE orders ALTER order_id SET DEFAULT nextval('order_id_seq');
@@ -76,5 +65,18 @@ ALTER TABLE shipping_company ALTER shipping_company_id SET DEFAULT nextval('ship
 ALTER SEQUENCE shipping_company_id_seq OWNED BY shipping_company.shipping_company_id;
 
 --j. State
+SELECT max(state_id) +1 FROM state;
+CREATE SEQUENCE state_id_seq MINVALUE 7;
+ALTER TABLE state ALTER state_id SET DEFAULT nextval('state_id_seq');
+ALTER SEQUENCE state_id_seq OWNED BY state.state_id;
+
+--testing
+SELECT * FROM state;
+INSERT INTO state (name, abbreviation)
+VALUES ('Bahia', 'BH');
 
 --k. Salesperson
+SELECT max(salesperson_id) + 1 FROM salesperson;
+CREATE SEQUENCE salesperson_id_seq MINVALUE 9;
+ALTER TABLE salesperson ALTER salesperson_id SET DEFAULT nextval('salesperson_id_seq');
+ALTER SEQUENCE salesperson_id_seq OWNED BY salesperson.salesperson_id;
