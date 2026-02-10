abc4afa248420dcbbd675a9917a7ac6665fb18d3
ricardo.shinoda <ricardoshinoda@gmail.com>
2026-02-09 21:09:01 -0300

    procedure exercise 76.2

--- exercise-76-procedure.sql
+++ exercise-76-procedure.sql
@@ -15,3 +15,33 @@ DROP PROCEDURE increase_value;
 
 
 --2. Create a stored procedure that receives as parameter the product ID and deletes from the database only the product with the corresponding ID.
+
+
+CREATE TABLE test (
+	test_id integer NOT NULL,
+	name varchar(20) NOT NULL
+);
+
+INSERT INTO test (test_id, name)
+VALUES (1, 'test1'),
+(2, 'test2'),
+(3, 'test3'),
+(4, 'test4');
+
+SELECT * FROM test;
+
+-- creating with a test table
+
+CREATE PROCEDURE test_del (tst_id integer) LANGUAGE SQL AS
+$$
+	delete from test where test_id = tst_id;
+$$;
+
+CALL test_del(3);
+
+CREATE PROCEDURE pdt_del (prd_id integer) LANGUAGE SQL AS
+$$
+	delete from product where product_id = prd_id;
+$$;
+
+CALL pdt_del(7);
