b211eb04262221f82079844727a0d92b90f011ea
ricardo.shinoda <ricardoshinoda@gmail.com>
2026-02-12 08:25:41 -0300

    refactoring user & permision exercise

--- exercse-85-users-permissions.sql
+++ exercse-85-users-permissions.sql
@@ -8,10 +8,16 @@ GRANT SELECT, INSERT ON
 	order_product
 TO clerk WITH GRANT OPTION;
 
+-- to enable the insert into table, due to an error on order_id_seq
+GRANT ALL ON order_id_seq TO clerk WITH GRANT OPTION;
+
 --3. Create a new user associated with the new role
 CREATE ROLE jose login PASSWORD '0000' IN ROLE clerk;
 
 --4. Perform tests to verify that the permissions were applied correctly
-
 SELECT * FROM orders; -- select ok.
 SELECT * FROM customer; -- permission denied for table customer 
+
+-- Testing insert
+INSERT INTO orders (customer_id, shipping_company_id, salesperson_id, order_date, total_value)
+VALUES (1, 1, 1, '2024-01-01', 500);
